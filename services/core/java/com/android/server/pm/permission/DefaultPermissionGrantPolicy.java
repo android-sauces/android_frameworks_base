e3fb5b349399c39c3df4db875246c99f43f9097a
Josh Chasky <jchasky@gmail.com>
2016-10-30 21:37:15 -0400

    Fix Google Calendar FC
    * Reported by many users using full dynamic gapps pkg
    ** Also give some other apps default permissions
    
    [@AgentFabulous - POSP]
    - Adapt for android-10.0.0_r2
    - Use grantSystemFixedPermissionsToSystemPackage to simplify syntax
    
    Change-Id: I88855594a080dd34d592e759b9a94a5f5d44e104
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: Sebita <kjjjnob.seba@gmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -791,12 +791,14 @@ public final class DefaultPermissionGrantPolicy {
         grantSystemFixedPermissionsToSystemPackage("com.google.android.apps.tycho", userId, CONTACTS_PERMISSIONS,
                 PHONE_PERMISSIONS, MICROPHONE_PERMISSIONS, ALWAYS_LOCATION_PERMISSIONS, SMS_PERMISSIONS);
 
-
         // ContactsProvider2
         grantSystemFixedPermissionsToSystemPackage(
                 getDefaultProviderAuthorityPackage("com.android.providers.contacts.ContactsProvider2", userId), userId,
                 CONTACTS_PERMISSIONS, STORAGE_PERMISSIONS);
 
+        // Google Calendar
+        grantSystemFixedPermissionsToSystemPackage("com.google.android.calendar", userId, CALENDAR_PERMISSIONS,
+                CONTACTS_PERMISSIONS, PHONE_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(String category, int userId) {
